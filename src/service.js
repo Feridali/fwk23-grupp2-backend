c17482862cc0b3fdd9c323a09ecaf4d34f691778
Feridali <ferid62@hotmail.com>
2024-01-29 13:47:16 +0100

    update

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
-require ('dotenv').config().
-cost app = ('/server.js');
+require ('dotenv').config();
+const app = ('/server.js');
 const PORT = process.env.PORT || 3002;
 
 app.listen (PORT, () => {
-  console.log(`http server listening on port ${PORT}`);
+  console.log(`http server listening on port ${PORT}`)
 });
